6f35bf4619478ddd2c127a0cf22f489fb014a469
Ankush Mehta <mail@nksh.io>
2019-01-16 06:49:22 +0530

    Modifies webpack config to uglify directive

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 var path = require('path');
+var webpack = require('webpack');
+
+var debug = process.env.NODE_ENV !== "PROD";
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.js'),
   output: {
     path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    filename: debug ? 'bundle.js' : 'bundle.min.js',
   },
   module: {
     loaders: [
@@ -14,4 +17,13 @@ module.exports = {
       },
     ]
   },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+        mangle: true,
+        minimize: true,
+        compress: true
+    }),
+],
 };
